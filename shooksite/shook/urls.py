fb6f442fab436b2f4fa7e27f6135658fcff5b994
Zakir Butte <zakir.butte@gmailcom>
2018-04-09 10:59:42 -0700

    auth token working on backend

--- shooksite/shook/urls.py
+++ shooksite/shook/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 #added for Token
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
+urlpatterns = router.urls
 
-urlpatterns = [
+urlpatterns += [
     path('shook/', views.LeadListCreate.as_view() ),
     path('<int:pk>/', views.LeadDetail.as_view()),
     re_path(r'^obtain-auth-token/$', obtain_auth_token),
