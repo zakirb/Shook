eff52f1743fa9c64563c1bfa19834231876ea199
Daniel Heyward <danheyward@gmail.com>
2018-04-11 10:52:04 -0700

    readding commit

--- shooksite/frontend/src/components/UserBadge.js
+++ shooksite/frontend/src/components/UserBadge.js
@@ -7,7 +7,7 @@ const UserBadge = data => {
   return (
     <div className='user-badge'>
       <Card className='small'
-        header={<CardTitle image='img/sample-1.jpg'>{data}</CardTitle>}
+        header={<CardTitle image='img/sample-1.jpg'>Card Title</CardTitle>}
         actions={[<a href='#'>This is a Link</a>]}>
         I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
       </Card>
