879ff08b5cc739c5a63943ff6b7277d6d91b89a6
Scott Molsness <smolsness@gmail.com>
2018-04-12 16:53:35 -0700

    darkened bonfire image to make text more readable

--- shooksite/frontend/urls.py
+++ shooksite/frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, re_path
 from . import views
 urlpatterns = [
     path('', views.index ),
-    re_path(r'^\w*/', views.index )
+    path('profile/', views.index),
+    path('login/', views.index),
+    path('signup/', views.index),
 ]
